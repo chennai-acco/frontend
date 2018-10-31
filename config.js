8499e295695055fd246745559a23cf1ca4ca1396
VimalRaj Selvam <email2vimalraj@gmail.com>
2018-10-31 22:36:41 +0800

    Added the prod server endpoint

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`
+export const prodEndpoint = `https://chennai-acco-yoga-prod.herokuapp.com/`
 export const perPage = 4
 export const cloudinaryEndpoint = `https://api.cloudinary.com/v1_1/vimal-wesbos-tutorial/image/upload`
